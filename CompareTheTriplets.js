e3082f787d9aafcb490558598827398757f50eac
JuneKR <jdei_nin230@hotmail.com>
2019-07-07 10:54:01 +0700

    CompareTheTriplets.js

---
+++ CompareTheTriplets.js
@@ -0,0 +1,20 @@
+function compareTriplets(a, b) {
+    var Alice = 0;
+    var Bob = 0;
+    for (var i = 0; i < a.length; i++) {
+        if (a[i] > b[i]) {
+            Alice += 1;
+        }
+        else if (a[i] == b[i]) {
+            Alice += 0;
+            Bob += 0;
+        }
+        else {
+            Bob += 1;
+        }
+    }
+    // console.log(Alice + ' '+ Bob);
+    return Alice + ' ' + Bob;
+}
+
+// compareTriplets([5,6,7], [3,6,10]);
